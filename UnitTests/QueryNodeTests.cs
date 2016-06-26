2639e0e18cca9a5fbc0ed1b1dc469f4214797bf4
Andrew Jones <ajon542@gmail.com>
2016-06-26 10:46:11 -0700

    Added QueryNodes tests
    Fixes #7

---
+++ UnitTests/QueryNodeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SegmentTree;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class QueryNodeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SegmentTree.SegmentTree tree = new SegmentTree.SegmentTree();
+            tree.Build(new List<int> { 1 });
+
+            Assert.AreEqual(1, tree.QueryNodes(new Range<int>(0, 0)));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SegmentTree.SegmentTree tree = new SegmentTree.SegmentTree();
+            tree.Build(new List<int> { 1, 2 });
+
+            Assert.AreEqual(3, tree.QueryNodes(new Range<int>(0, 1)));
+            Assert.AreEqual(1, tree.QueryNodes(new Range<int>(0, 0)));
+            Assert.AreEqual(2, tree.QueryNodes(new Range<int>(1, 1)));
+        }
+    }
+}
